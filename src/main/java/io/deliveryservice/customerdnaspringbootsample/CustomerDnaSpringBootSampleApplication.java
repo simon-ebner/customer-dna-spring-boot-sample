80d4eced0b992a03a03488d06a1d1f75ba9ce00a
Simon Ebner <mail@simon-ebner.de>
2021-01-10 01:57:04 +0100

    initial commit

---
+++ src/main/java/io/deliveryservice/customerdnaspringbootsample/CustomerDnaSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package io.deliveryservice.customerdnaspringbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDnaSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDnaSpringBootSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/deliveryservice/customerdnaspringbootsample/CustomerDnaSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.deliveryservice.customerdnaspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDnaSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
